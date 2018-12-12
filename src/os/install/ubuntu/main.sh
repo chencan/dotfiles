b50a2fa3abcf30e7566ccf644a52c96971745176
Can Chen <can.chen@nio.com>
2018-12-12 18:32:42 +0800

    not install some tools

--- src/os/install/ubuntu/main.sh
+++ src/os/install/ubuntu/main.sh
@@ -12,11 +12,11 @@ upgrade
 ./build-essentials.sh
 ./../nvm.sh
 
-./browsers.sh
+#./browsers.sh
 ./compression_tools.sh
 ./git.sh
-./image_tools.sh
-./misc.sh
+#./image_tools.sh
+#./misc.sh
 ./misc_tools.sh
 ./../npm.sh
 ./tmux.sh
